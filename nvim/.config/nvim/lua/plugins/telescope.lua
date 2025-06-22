8b9e8d03590dcf0f022795a1d97f9f7dd46984e8
Pevin Elsenique <pevin.elsenique@shippit.com>
2025-06-22 18:36:04 +0800

    Add telescope live grep args dependency

--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -7,6 +7,8 @@ return {
     { -- If encountering errors, see telescope-fzf-native README for installation instructions
       'nvim-telescope/telescope-fzf-native.nvim',
 
+      'nvim-telescope/telescope-live-grep-args.nvim',
+
       -- `build` is used to run some command when the plugin is installed/updated.
       -- This is only run then, not every time Neovim starts up.
       build = 'make',
@@ -45,6 +47,9 @@ return {
     -- [[ Configure Telescope ]]
     -- See `:help telescope` and `:help telescope.setup()`
     local actions = require 'telescope.actions'
+    local telescope = require 'telescope'
+    local lga_actions = require 'telescope-live-grep-args.actions'
+
     require('telescope').setup {
       -- You can put your default mappings / updates / etc. in here
       --  All the info you're looking for is in `:help telescope.setup()`
@@ -62,11 +67,13 @@ return {
         },
         mappings = {
           i = {
-            -- ['<C-k>'] = actions.move_selection_previous, -- move to prev result
-            -- ['<C-j>'] = actions.move_selection_next, -- move to next result
+            ['<C-p>'] = actions.move_selection_previous, -- move to prev result
+            ['<C-n>'] = actions.move_selection_next, -- move to next result
             ['<C-y>'] = actions.select_default, -- open file
-            ['<C-e>'] = actions.select_default, -- open file
-            ['<esc>'] = actions.close,
+            ['<C-e>'] = actions.select_default, -- open file alternative for easier reach
+          },
+          n = {
+            ['<C-c>'] = actions.close,
           },
         },
       },
@@ -86,12 +93,24 @@ return {
         ['ui-select'] = {
           require('telescope.themes').get_dropdown(),
         },
+        live_grep_args = {
+          auto_quoting = true, -- enables -F and quoting
+          mappings = { -- optional: add custom keymaps
+            i = {
+              ['<C-k>'] = lga_actions.quote_prompt(),
+              ['<C-i>'] = lga_actions.quote_prompt { postfix = ' --iglob ' },
+              ['<C-a>'] = lga_actions.to_fuzzy_refine,
+            },
+          },
+        },
       },
     }
 
     -- Enable Telescope extensions if they are installed
-    pcall(require('telescope').load_extension, 'fzf')
-    pcall(require('telescope').load_extension, 'ui-select')
+    pcall(telescope.load_extension, 'fzf')
+    pcall(telescope.load_extension, 'ui-select')
+
+    telescope.load_extension 'live_grep_args'
 
     -- See `:help telescope.builtin`
     local builtin = require 'telescope.builtin'
@@ -100,7 +119,15 @@ return {
     vim.keymap.set('n', '<leader>sf', builtin.find_files, { desc = '[S]earch [F]iles' })
     vim.keymap.set('n', '<leader>ss', builtin.builtin, { desc = '[S]earch [S]elect Telescope' })
     vim.keymap.set('n', '<leader>sw', builtin.grep_string, { desc = '[S]earch current [W]ord' })
-    vim.keymap.set('n', '<leader>se', builtin.live_grep, { desc = '[S]earch by gr[E]p' })
+    -- vim.keymap.set('n', '<leader>se', ":lua require('telescope').extensions.live_grep_args.live_grep_args()<CR>", { desc = '[S]earch by gr[E]p' })
+    vim.keymap.set('n', '<leader>se', function()
+      require('telescope').extensions.live_grep_args.live_grep_args {
+        additional_args = function()
+          return { '--hidden' }
+        end,
+      }
+    end, { desc = '[S]earch by gr[E]p' })
+    -- vim.keymap.set('n', '<leader>se', builtin.live_grep, { desc = '[S]earch by gr[E]p' })
     vim.keymap.set('n', '<leader>sd', builtin.diagnostics, { desc = '[S]earch [D]iagnostics' })
     vim.keymap.set('n', '<leader>sr', builtin.resume, { desc = '[S]earch [R]esume' })
     vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
