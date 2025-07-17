2dde4fa8cc1205d1508ea185d4f7585dfbbb9d9c
Pevin Elsenique <pevin.elsenique@shippit.com>
2025-07-17 09:31:53 +0800

    Disable auto format in formatter and added pl

--- nvim/.config/nvim/lua/plugins/formatter.lua
+++ nvim/.config/nvim/lua/plugins/formatter.lua
@@ -9,6 +9,15 @@ return {
         lua = {
           require('formatter.filetypes.lua').stylua,
         },
+        javascript = {
+          require('formatter.filetypes.javascript').prettier,
+        },
+        typescript = {
+          require('formatter.filetypes.typescript').prettier,
+        },
+        typescriptreact = {
+          require('formatter.filetypes.typescriptreact').prettier,
+        },
         ruby = {
           require('formatter.filetypes.ruby').rubocop,
         },
@@ -18,11 +27,14 @@ return {
       },
     }
 
-    vim.api.nvim_create_autocmd('BufWritePost', {
-      desc = 'Auto format on save',
-      group = vim.api.nvim_create_augroup('_local_auto_format', { clear = true }),
-      pattern = '*',
-      command = ':FormatWrite',
-    })
+    vim.keymap.set('n', '<leader>f', '<cmd>Format<CR>', { silent = true }) -- close current split window
+
+    -- DISABLE AUTO FORMATTER
+    -- vim.api.nvim_create_autocmd('BufWritePost', {
+    --   desc = 'Auto format on save',
+    --   group = vim.api.nvim_create_augroup('_local_auto_format', { clear = true }),
+    --   pattern = '*',
+    --   command = ':FormatWrite',
+    -- })
   end,
 }
