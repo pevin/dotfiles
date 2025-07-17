ee8c37bb1682254d1cbe607a69ef7b64f582686d
Pevin Elsenique <pevin.elsenique@shippit.com>
2025-07-17 09:32:22 +0800

    Disable none ls

--- nvim/.config/nvim/lua/plugins/none-ls.lua
+++ nvim/.config/nvim/lua/plugins/none-ls.lua
@@ -1,5 +1,6 @@
 return {
   'nvimtools/none-ls.nvim',
+  enable = false,
   dependencies = {
     'nvimtools/none-ls-extras.nvim',
     'jayp0521/mason-null-ls.nvim', -- ensure dependencies are installed
@@ -37,13 +38,13 @@ return {
       on_attach = function(client, bufnr)
         if client.supports_method 'textDocument/formatting' then
           vim.api.nvim_clear_autocmds { group = augroup, buffer = bufnr }
-          vim.api.nvim_create_autocmd('BufWritePre', {
-            group = augroup,
-            buffer = bufnr,
-            callback = function()
-              vim.lsp.buf.format { async = false }
-            end,
-          })
+          -- vim.api.nvim_create_autocmd('BufWritePre', {
+          --   group = augroup,
+          --   buffer = bufnr,
+          --   callback = function()
+          --     vim.lsp.buf.format { async = false }
+          --   end,
+          -- })
         end
       end,
     }
